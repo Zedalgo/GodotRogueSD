eaaa06fc63a5e1e3cb2d358cbfaf1aff13807aab
Zedalgo <xero321@verizon.net>
2022-01-27 15:10:42 -0500

    working on enemy movement and attacking

--- scene/main/MainScene.gd
+++ scene/main/MainScene.gd
@@ -6,20 +6,21 @@ var _player_scene:PackedScene = preload("res://sprite/Player.tscn")
 var _dwarf_scene:PackedScene = preload("res://sprite/Dwarf.tscn")
 var _corpse_scene:PackedScene = preload("res://sprite/Corpse.tscn")
 
-
 var _new_GetCoord = preload("res://library/GetCoord.gd").new()
 
 var player:Sprite
 var wall:Sprite
 
+
 var entities:Array = []
+var items:Array = []
 var map:Array = []
 
 var map_generated:bool = false
+var player_turn:bool = true
 
 var tile_width:int = 16
 var tile_height:int = 24
-
 
 # Theoretically runs when the main scene happens for the first time
 func _ready():
@@ -33,19 +34,22 @@ func _unhandled_input(Input):
 		print("Generating map...")
 		generate_map()
 		print("Map generated.")
-	if map_generated == true:
+	if map_generated == true && player_turn == true:
 		if Input.is_action_pressed("select"):
 			get_clicked_grid_tile()
 		if Input.is_action_pressed("debug_key"):
 			print(_new_GetCoord.vector_to_index(player.position.x, player.position.y))
 		if Input.is_action_pressed("move_down"):
-			try_move(0, 1)
+			try_move(player, 0, 1)
 		if Input.is_action_pressed("move_up"):
-			try_move(0, -1)
+			try_move(player, 0, -1)
 		if Input.is_action_pressed("move_right"):
-			try_move(1, 0)
+			try_move(player, 1, 0)
 		if Input.is_action_pressed("move_left"):
-			try_move(-1, 0)
+			try_move(player, -1, 0)
+		
+		if player_turn == false:
+			enemy_phase()
 
 
 # Currently generates a static map
@@ -67,12 +71,6 @@ func generate_map():
 				get_parent().add_child(_floor)
 				map[i].append(_floor)
 	
-	# Turns the walls red via Color(R, G, B), floats[0-1]
-#	for i in range(33):
-#		for j in range(17):
-#			if map[i][j].walkable == false:
-#				map[i][j].modulate = Color(1, 0, 0)
-	
 	player = _player_scene.instance()
 	player.position = _new_GetCoord.index_to_vector(3, 3)
 	get_parent().add_child(player)
@@ -83,10 +81,7 @@ func generate_map():
 		dwarf.entity_name = "dwarf"
 		dwarf.position = _new_GetCoord.index_to_vector(round(rand_range(1, 32)), round(rand_range(1, 15)))
 		get_parent().add_child(dwarf)
-		print(dwarf.name)
 		entities.append(dwarf)
-	
-	print(entities)
 
 # Identifies any entities in a grid index tile and prints them to the console
 # Creatures use .entity_name instead of .name because godot won't let two nodes share a .name
@@ -100,9 +95,9 @@ func get_clicked_grid_tile():
 			entity = "a %s " % entities[i].entity_name
 	print("There is ", entity)
 
-func try_move(dx, dy):
-	var try_vec_x = player.position.x + (dx * tile_width)
-	var try_vec_y = player.position.y + (dy * tile_height)
+func try_move(entity, dx, dy):
+	var try_vec_x = entity.position.x + (dx * tile_width)
+	var try_vec_y = entity.position.y + (dy * tile_height)
 	var try_index:Vector2 = _new_GetCoord.vector_to_index(try_vec_x, try_vec_y)
 	if map[try_index.x][try_index.y].walkable == false:
 		print("That's a wall")
@@ -118,8 +113,9 @@ func try_move(dx, dy):
 			print("Something's in the way, and you strike it!")
 			attack(player, blocking_entity)
 		else:
-			player.position.x = try_vec_x
-			player.position.y = try_vec_y
+			entity.position.x = try_vec_x
+			entity.position.y = try_vec_y
+			player_turn = false
 		
 		entity_is_blocking = false
 
@@ -131,10 +127,28 @@ func attack(attacker, defender):
 		corpse.position = defender.position
 		corpse.entity_name = "%s corpse" % defender.entity_name
 		get_parent().add_child(corpse)
-		entities.append(corpse)
+		items.append(corpse)
 		# The godot node isn't removed by the next line, so I'll need to find a way to do something about that later
 		defender.visible = false
 		for i in range(entities.size()):
 			if entities[i].position == defender.position:
 				entities.remove(i)
 				break
+
+func enemy_phase():
+	var direction:int
+	for i in range(1, entities.size()):
+		print(entities.size()) 
+		direction = round(rand_range(4,5))
+		if direction == 0:
+			try_move(entities[i], 0, -1)
+		if direction == 1:
+			try_move(entities[i], 0, 1)
+		if direction == 2:
+			try_move(entities[i], -1, 0)
+		if direction == 3:
+			try_move(entities[i], 1, 0)
+		if direction > 3:
+			pass
+		attack(entities[i], player)
+	player_turn = true
--- sprite/Player.gd
+++ sprite/Player.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
-export var entity_name:String = "Player"
 export var health:int = 20
 export var damage:int = 10
+
+export var entity_name:String = "Player"
+
+var walkable = false
